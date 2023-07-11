176fe4325cc5288ed1dc55e17aa5adda8eaffa62
nLog0312 <105146353+nLog0312@users.noreply.github.com>
2023-07-11 11:40:30 +0700

    update giao dien khach hang

--- customer/content/modal_signup.php
+++ customer/content/modal_signup.php
@@ -1,19 +1,22 @@
 <div id="signup" class="modal">
-    <form class="modal-content animate" action="sginup.php" method="post">
+    <form class="modal-content animate" action="signup.php" method="post">
         <div class="imgcontainer">
             <span onclick="document.getElementById('signup').style.display='none'" class="close" title="Close Modal">&times;</span>
         </div>
 
         <div class="container-main">
             <div class="container-top">
+                <label for="username"><b>Tên đăng nhập</b></label>
+                <input type="text" placeholder="Tên đăng nhập..." name="username" required>
+
                 <label for="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="email" required>
+                <input type="text" placeholder="Email..." name="email" required>
 
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required>
+                <label for="psw"><b>Mật khẩu</b></label>
+                <input type="password" placeholder="Mật khẩu..." name="psw" required>
 
-                <label for="psw-repeat"><b>Repeat Password</b></label>
-                <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+                <label for="psw-repeat"><b>Nhập lại mật khẩu</b></label>
+                <input type="password" placeholder="Nhập lại mật khẩu..." name="psw-repeat" required>
                 
                 <label>
                     <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
